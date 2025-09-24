bda2cd76721c7514ac3d87e25972128d17326f84
Vikas Joshi <145147066+vicky262600@users.noreply.github.com>
2025-09-24 15:50:54 -0400

    whatsApp Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WhatsAppSticky from "../components/WhatsAppSticky";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <WhatsAppSticky />
       </body>
     </html>
   );
